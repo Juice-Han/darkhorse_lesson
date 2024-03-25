9d23ed50bb6923a46cfc73c86e01212eaa6706b4
JuHan Park <iiiamst16@gmail.com>
2024-03-25 18:09:45 +0900

    bootstrap 적용

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(8080,()=>{
     console.log('Listening on 8080 port');
