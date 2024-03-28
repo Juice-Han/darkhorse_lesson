7407cd43b829102090a4e3bd996646b63e58f1a7
JuHan Park <iiiamst16@gmail.com>
2024-03-28 13:44:44 +0900

    신청페이지 map으로 코드 줄이기

--- app.js
+++ app.js
@@ -2,43 +2,44 @@ const express = require('express');
 const path = require('path')
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log('Listening on 8080 port');
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('login.ejs');
 })
 
-app.get('/user/register',(req,res)=>{
+app.get('/user/register', (req, res) => {
     res.render('register.ejs');
 })
 
-app.post('/user/register',(req,res)=>{
+app.post('/user/register', (req, res) => {
     res.render('login.ejs');
 })
 
-app.post('/user/login',(req,res)=>{
+app.post('/user/login', (req, res) => {
     //로그인 확인 코드 작성
     res.redirect('/choose');
 })
 
-app.get('/choose', (req,res)=>{
+app.get('/choose', (req, res) => {
     //로그인 정보 세션 없으면 들어가지 접근 못하게 막는 코드 작성
-    res.render('choose.ejs');
+    const dayList = ['월요일', '화요일', '수요일', '목요일', '금요일']
+    res.render('choose.ejs', { dayList });
 })
 
-app.get('/choose/:day',(req,res)=>{
+app.get('/choose/:day', (req, res) => {
     let day = 0;
-    if(req.params.day === '0') day = '월요일';
-    else if(req.params.day === '1') day = '화요일';
-    else if(req.params.day === '2') day = '수요일';
-    else if(req.params.day === '3') day = '목요일';
-    else if(req.params.day === '4') day = '금요일';
-    
+    if (req.params.day === '0') day = '월요일';
+    else if (req.params.day === '1') day = '화요일';
+    else if (req.params.day === '2') day = '수요일';
+    else if (req.params.day === '3') day = '목요일';
+    else if (req.params.day === '4') day = '금요일';
+
     //요일 별 타임 리스트 작성해야함
-    const timeList = ['10:30 ~ 10:45','10:45 : 11:00', '11:00 ~ 11:15', '11:15 ~ 11:30'];
-    res.render('chooseDetail.ejs',{ day, timeList });
+    const timeList = ['10:30 ~ 10:45', '10:45 : 11:00', '11:00 ~ 11:15', '11:15 ~ 11:30'];
+    res.render('chooseDetail.ejs', { day, timeList });
 })
